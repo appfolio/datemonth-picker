7bfba9b430f95c1b8c5e90fe59a6d93819170fe4
Gary Thomas <gary.thomas@appfolio.com>
2016-06-29 23:33:25 -0700

    Add nightmarejs tests instead of zombie

--- test/browser.spec.js
+++ test/browser.spec.js
@@ -1,22 +1,102 @@
 /* eslint-env mocha */
 
-import Browser from 'zombie';
+import assert from 'assert';
+import Nightmare from 'nightmare';
 import server from '../dev_server.js';
 
-Browser.localhost('example.com', 8080);
-
 describe('Render component', () => {
-  const browser = new Browser();
-  browser.waitFor = 1000;
+  const browser = (new Nightmare()).goto('http://localhost:8080');
 
-  before(done => {
-    browser.visit('/', done);
+  it('should prepopulated with initial value', () => {
+    return browser
+      .evaluate(() => document.querySelector('input[name="datemonth"]').value)
+      .then(value => assert.equal(value, 'Nov 1971'));
   });
 
-  it('should be prepopulated with initial value and closed', () => {
-    browser.assert.input('input[name="datemonth"]', 'Nov 1971');
-    browser.assert.elements('.picker', 0);
+  it('should be closed', () => {
+    return browser
+      .visible('.picker')
+      .then(exists => assert(!exists, 'picker should be closed'));
   });
 
-  // TODO open/close, pick dates, verify values, etc
+  it('should open when clicked', () => {
+    return browser
+      .click('.toggle')
+      .visible('.picker')
+      .then(exists => assert(exists, 'picker did not open'));
+  });
+
+  it('should close when clicked', () => {
+    return browser
+      .click('.toggle')
+      .visible('.picker')
+      .then(exists => assert(!exists, 'picker did not close'));
+  });
+
+  it('should open when focused', () => {
+    return browser
+      .insert('input[name="datemonth"]')
+      .visible('.picker')
+      .then(exists => assert(exists, 'picker did not open'));
+  });
+
+  it('should update value when selecting month', () => {
+    return browser
+      .click('li[data-month="Jan"]')
+      .evaluate(() => document.querySelector('input[name="datemonth"]').value)
+      .then(value => assert.equal(value, 'Jan 1971'));
+  });
+
+  it('should update value when selecting year', () => {
+    return browser
+      .click('li[data-year="1976"]')
+      .evaluate(() => document.querySelector('input[name="datemonth"]').value)
+      .then(value => assert.equal(value, 'Jan 1976'));
+  });
+
+  it('should update value when selecting forward', () => {
+    return browser
+      .click('#next')
+      .evaluate(() => document.querySelector('input[name="datemonth"]').value)
+      .then(value => assert.equal(value, 'Jan 1986'));
+  });
+
+  it('should update value when selecting previous', () => {
+    return browser
+      .click('#prev')
+      .click('#prev')
+      .evaluate(() => document.querySelector('input[name="datemonth"]').value)
+      .then(value => assert.equal(value, 'Jan 1966'));
+  });
+
+  it('should revert when clicking cancel', () => {
+    return browser
+      .click('#cancel')
+      .evaluate(() => document.querySelector('input[name="datemonth"]').value)
+      .then(value => assert.equal(value, 'Nov 1971'));
+  });
+
+  it('should update value when clicking OK', () => {
+    return browser
+      .click('.toggle')
+      .click('li[data-month="Mar"]')
+      .click('#save')
+      .evaluate(() => document.querySelector('input[name="datemonth"]').value)
+      .then(value => assert.equal(value, 'Mar 1971'));
+  });
+
+  it('should stop at current year when clicking into future', () => {
+    const now = new Date();
+    return browser
+      .click('.toggle')
+      .click('li[data-month="Jun"]')
+      .click('#next')
+      .click('#next')
+      .click('#next')
+      .click('#next')
+      .click('#next')
+      .click('#next')
+      .evaluate(() => document.querySelector('input[name="datemonth"]').value)
+      .then(value => assert.equal(value, `Jun ${now.getFullYear()}`));
+  });
 });
