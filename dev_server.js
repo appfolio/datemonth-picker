a66ccdf7b77b143944bbda3860a88d24b29ce9a3
Gary Thomas <gary.thomas@appfolio.com>
2016-07-14 12:38:21 -0700

    Quiet dev server console

--- dev_server.js
+++ dev_server.js
@@ -12,7 +12,7 @@ const config = require('./webpack.config.js');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const compiler = webpack(config);
-app.use(webpackDevMiddleware(compiler, { publicPath: config.output.publicPath }));
+app.use(webpackDevMiddleware(compiler, { publicPath: config.output.publicPath, noInfo: true }));
 
 const port = 8080 || process.env.port;
 app.listen(port, () => console.log(`Listening on ${port}`));
