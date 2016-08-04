5acbce13215c5f716604d11936a428ff9a9a3d6a
Gary Thomas <gary.thomas@appfolio.com>
2016-08-03 23:44:06 -0700

    gt - Handle bad input gracefully

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import fecha from 'fecha';
 
 // CJS-style export wrapper to avoid `DateMonth.default`:
 module.exports = (element, name = '', date = new Date()) => {
-  let initialDate = date;
+  let initialDate = new Date();
   if (date && typeof date === 'string') {
-    initialDate = fecha.parse(date, 'MMM YYYY');
+    const parsedDate = fecha.parse(date, 'MMM YYYY');
+    if (parsedDate) {
+      initialDate = parsedDate;
+    }
   }
   const month = fecha.format(initialDate, 'MMM');
   const year = initialDate.getFullYear();
