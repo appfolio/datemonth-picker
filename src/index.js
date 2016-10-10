905f64f6479e7272a6fb36ef7c9b844b69b7f9b8
Gary Thomas <gary.thomas@appfolio.com>
2016-10-10 14:44:08 -0700

    gt - Revert webcomponent
    Revert for now until filter modal error is resolved

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { h, render } from 'preact';
 import DateMonth from './DateMonth.js';
-import register from 'preact-custom-element';
 
-register(DateMonth, 'af-datemonth');
+// CJS-style export wrapper to avoid `DateMonth.default`:
+module.exports = (element, name = '', date, required = false) => {
+  render(<DateMonth name={name} value={date} required={required} />, element.parentElement, element);
+};
 
 if (module.hot) {
   module.hot.accept();
